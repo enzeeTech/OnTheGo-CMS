36f8ec8ba91dcfb55a00192b176e51624729245b
Benjamin Irfan <benjaminirfan.work@gmail.com>
2024-01-22 10:37:42 +0800

    updating admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: 'AR8b2OFqxt6/GE2SUaFwlg==',
+    secret: 'ebbde954212dde7e7876fb5152e0496905221145bb0580e762f41c73df2f1b97',
   },
   apiToken: {
-    salt: 'ZLvMxKlEVp6bw4IG/sU7ZA==',
+    salt: '7f3febf6c670e48a37df40769983cb48b2e334f02707b333566cf63e7ca39a75',
   },
   transfer: {
     token: {
-      salt: '4py0aZzXw89XfPfT5gHRjQ==',
+      salt: 'dc3296fd373ec0780ae08092546bfec533c96faebb537a9441ae804cc4862e1a',
     },
   },
   flags: {
