b22980a78a5b596d2d722a002cf537c585a4384b
Benjamin Irfan <benjaminirfan.work@gmail.com>
2024-01-22 10:25:25 +0800

    changed the salt for auth, apiToken and transfer token

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: 'AR8b2OFqxt6/GE2SUaFwlg',
+    secret: 'AR8b2OFqxt6/GE2SUaFwlg==',
   },
   apiToken: {
-    salt: 'ZLvMxKlEVp6bw4IG/sU7ZA',
+    salt: 'ZLvMxKlEVp6bw4IG/sU7ZA==',
   },
   transfer: {
     token: {
-      salt: '4py0aZzXw89XfPfT5gHRjQ',
+      salt: '4py0aZzXw89XfPfT5gHRjQ==',
     },
   },
   flags: {
